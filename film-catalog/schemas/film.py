bf21d70d90b2a774bb535904b6c565b2c49edfd4
Khuzin Rustam <ligreman@mail.ru>
2025-05-09 20:05:12 +0300

    add film create schema

--- film-catalog/schemas/film.py
+++ film-catalog/schemas/film.py
@@ -1,13 +1,26 @@
+from typing import Annotated
+from datetime import datetime
+from annotated_types import Len, Interval
 from pydantic import BaseModel
 
 
 class FilmBase(BaseModel):
-    id: int
     name: str
     description: str
     year: int
-    rating: float
 
 
 class Film(FilmBase):
-    pass
+    id: int
+    rating: float
+
+
+class FilmCreate(FilmBase):
+    name: Annotated[
+        str,
+        Len(min_length=1, max_length=100),
+    ]
+    year: Annotated[
+        int,
+        Interval(ge=1895, le=datetime.now().year + 5),
+    ]
