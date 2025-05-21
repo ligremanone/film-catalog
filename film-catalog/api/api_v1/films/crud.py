49237c61ddf3d6d5a0769a4488985e5c18aa1e01
Khuzin Rustam <ligreman@mail.ru>
2025-05-21 23:52:35 +0300

    add save data to json file

--- film-catalog/api/api_v1/films/crud.py
+++ film-catalog/api/api_v1/films/crud.py
@@ -1,10 +1,40 @@
+import json
+from pathlib import Path
+
 from pydantic import BaseModel
 
-from schemas.film import Film, FilmCreate, FilmUpdate, FilmUpdatePartial
+from core.config import DB_PATH
+from schemas.film import (
+    Film,
+    FilmCreate,
+    FilmUpdate,
+    FilmUpdatePartial,
+)
 
 
 class FilmCatalogStorage(BaseModel):
     slug_to_film: dict[str, Film] = {}
+
+    def save_data(self):
+        with open(
+            DB_PATH,
+            "w+",
+            encoding="utf-8",
+        ) as file:
+            json.dump(
+                self.model_dump(),
+                file,
+                indent=4,
+                ensure_ascii=False,
+            )
+
+    @classmethod
+    def from_data(cls) -> "FilmCatalogStorage":
+        if not Path(DB_PATH).exists():
+            return FilmCatalogStorage()
+        with open(DB_PATH, "r", encoding="utf-8") as file:
+            data = json.load(file)
+            return cls.model_validate(data)
 
     def get(self) -> list[Film]:
         return list(self.slug_to_film.values())
@@ -15,17 +45,20 @@ class FilmCatalogStorage(BaseModel):
     def create(self, new_film_in: FilmCreate) -> Film:
         new_film = Film(**new_film_in.model_dump(), rating=0)
         self.slug_to_film[new_film.slug] = new_film
+        self.save_data()
         return new_film
 
     def delete_by_slug(self, slug: str) -> None:
+        self.save_data()
         self.slug_to_film.pop(slug, None)
 
     def delete(self, film: Film) -> None:
         self.delete_by_slug(slug=film.slug)
 
-    def updade(self, film: Film, film_update: FilmUpdate) -> Film:
+    def update(self, film: Film, film_update: FilmUpdate) -> Film:
         for field_name, value in film_update:
             setattr(film, field_name, value)
+        self.save_data()
         return film
 
     def update_partial(
@@ -35,31 +68,12 @@ class FilmCatalogStorage(BaseModel):
             exclude_unset=True
         ).items():
             setattr(film, field_name, value)
+        self.save_data()
         return film
 
 
-storage = FilmCatalogStorage()
-storage.create(
-    FilmCreate(
-        slug="the-shawshank-redemption",
-        name="The Shawshank Redemption",
-        description="Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
-        year=1994,
-    )
-)
-storage.create(
-    FilmCreate(
-        slug="the-godfather",
-        name="The Godfather",
-        description="An organized crime dynasty's aging patriarch transfers control of his clandestine empire to his reluctant son.",
-        year=1972,
-    )
-)
-storage.create(
-    FilmCreate(
-        slug="the-dark-knight",
-        name="The Dark Knight",
-        description="When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one that he can't control.",
-        year=2008,
-    )
-)
+try:
+    storage = FilmCatalogStorage().from_data()
+except ValueError:
+    storage = FilmCatalogStorage()
+    storage.save_data()
--- film-catalog/api/api_v1/films/dependencies.py
+++ film-catalog/api/api_v1/films/dependencies.py
@@ -8,6 +8,7 @@ async def prefetch_film(slug: str) -> Film:
     film = storage.get_by_slug(slug)
     if film:
         return film
+
     raise HTTPException(
         status_code=status.HTTP_404_NOT_FOUND,
         detail=f"Film with {slug!r} slug not found",
