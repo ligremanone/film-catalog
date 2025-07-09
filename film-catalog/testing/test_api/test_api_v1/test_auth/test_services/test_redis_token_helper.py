2a993ec64492dab0d91b88a6dfb430395da98833
Khuzin Rustam <ligreman@mail.ru>
2025-07-09 14:34:49 +0500

    test redis tokens helper with db

---
+++ film-catalog/testing/test_api/test_api_v1/test_auth/test_services/test_redis_token_helper.py
@@ -0,0 +1,48 @@
+from os import getenv
+from unittest import TestCase
+
+from api.api_v1.auth.services import redis_tokens
+
+if getenv("TESTING") != "1":
+    msg = "Environment not ready for testing"
+    raise OSError(msg)
+
+
+class RedisTokensHelperTestCase(TestCase):
+    def test_generate_and_save_token(self) -> None:
+        new_token = redis_tokens.generate_and_save_token()
+        expected_exists = True
+        self.assertEqual(
+            expected_exists,
+            redis_tokens.token_exists(new_token),
+        )
+
+    def test_add_token(self) -> None:
+        new_token = redis_tokens.generate_token()
+        redis_tokens.add_token(new_token)
+        expected_exists = True
+        self.assertEqual(
+            expected_exists,
+            redis_tokens.token_exists(new_token),
+        )
+
+    def test_get_token(self) -> None:
+        tokens = redis_tokens.get_tokens()
+        self.assertTrue(tokens)
+        self.assertIsInstance(tokens, list)
+        self.assertGreater(len(tokens), 0)
+
+    def test_delete_token(self) -> None:
+        token = redis_tokens.generate_token()
+        redis_tokens.add_token(token)
+        expected_exists = True
+        self.assertEqual(
+            expected_exists,
+            redis_tokens.token_exists(token),
+        )
+        redis_tokens.delete_token(token)
+        expected_exists = False
+        self.assertEqual(
+            expected_exists,
+            redis_tokens.token_exists(token),
+        )
