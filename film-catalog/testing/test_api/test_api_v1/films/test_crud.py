8620e2c7691316c34c4256bad4876a70a9ecc084
Khuzin Rustam <ligreman@mail.ru>
2025-07-19 23:46:08 +0500

    pytest raises first look

--- film-catalog/testing/test_api/test_api_v1/films/test_crud.py
+++ film-catalog/testing/test_api/test_api_v1/films/test_crud.py
@@ -3,7 +3,9 @@ import string
 from typing import ClassVar
 from unittest import TestCase
 
-from api.api_v1.films.crud import storage
+import pytest
+
+from api.api_v1.films.crud import FilmAlreadyExistsError, storage
 from schemas.film import Film, FilmCreate, FilmUpdate, FilmUpdatePartial
 
 
@@ -94,3 +96,14 @@ class FilmStorageGetTestCase(TestCase):
                     film,
                     db_film,
                 )
+
+
+def test_create_or_raise_if_exists() -> None:
+    existing_film = create_film()
+    film_create = FilmCreate(**existing_film.model_dump())
+    with pytest.raises(
+        FilmAlreadyExistsError,
+        match=film_create.slug,
+    ) as exc_info:
+        storage.create_or_raise_if_exists(film_create)
+    assert exc_info.value.args[0] == film_create.slug
